f88285631ff3d32ba11554ce9bf104a67ec79331
Vladislav Nikolaev <vladisnotlove@gmail.com>
2023-03-13 17:20:18 +0300

    Add page loader

--- src/components/uiKit/Drawer/toggleDrawer.ts
+++ src/components/uiKit/Drawer/toggleDrawer.ts
@@ -1,10 +1,18 @@
+let timeoutId = -1;
+
 const toggleDrawer = (drawer: Element) => {
     drawer.classList.toggle("open");
+
+    if (timeoutId !== -1) window.clearTimeout(timeoutId);
 
     if (drawer.classList.contains("open")) {
         document.documentElement.classList.add("drawer-open");
     } else {
-        document.documentElement.classList.remove("drawer-open");
+        const styles = getComputedStyle(drawer);
+        const time = (parseFloat(styles.transitionDelay) + parseFloat(styles.transitionDuration)) * 1000;
+        timeoutId = window.setTimeout(() => {
+            document.documentElement.classList.remove("drawer-open");
+        }, time)
     }
 }
 
